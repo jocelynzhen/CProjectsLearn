55591e735d70cf569047629358b0fd6a245b0a01
Jocelyn Lee <jocelynlee977@gmail.com>
2023-04-17 23:37:55 +0800

    first commit

---
+++ comments.c
@@ -0,0 +1,14 @@
+// Program to print Hello World
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World!\n"); // print Hello World
+
+    return 0;
+}
+
+// Note that // is used for single-line comment
+
+/* This is multi-line comments.
+Therefore, it can be used for longer comments. */
---
+++ escape_sequence.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    //    \n in multiple lines of codes
+    printf("Hello!\n");
+    printf("This is me\n\n");
+
+    //    \n in one line of codes 
+    printf("Hello!\nThis is me\n\n");
+
+    //    \t Creates a horizontal tab
+    printf("Hello!\t This is me\n\n");
+
+    //    \\ Inserts a backslash character (\)
+    printf("Hello!\\ \n\n");
+
+    //    \" Inserts a double quote character
+    printf("Hello! I'm \"Jocelyn\"\n\n");
+    
+    return 0;
+}
